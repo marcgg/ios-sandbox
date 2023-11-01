e0c5cfb1535d42bb101a0227cc1d53f7ad04874d
Marc G Gauthier <marcg.gauthier@gmail.com>
2023-11-01 18:48:36 +0100

    Assistant example

--- cjem/ContentView.swift
+++ cjem/ContentView.swift
@@ -1,21 +1,65 @@
-//
-//  ContentView.swift
-//  cjem
-//
-//  Created by Marc G Gauthier on 01/11/2023.
-//
-
 import SwiftUI
 
+struct Bubble: View {
+    var text: String
+    var left: Bool
+    
+    var body: some View {
+        HStack {
+            if(!left) {
+                Spacer()
+            }
+            Text("\(text)")
+                .foregroundColor(.white)
+                .cornerRadius(5)
+                .padding()
+                .background(left ? .blue : .green)
+            if(left) {
+                Spacer()
+            }
+        }
+    }
+}
+
 struct ContentView: View {
+    @State var question: String = ""
+    @State var bubbles: Array<String> = []
+    @State var thinking: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("assistant")
+            ForEach(Array(bubbles.enumerated()), id: \.offset) { index, bubble in
+                Bubble(text: bubble, left: (index % 2 == 0))
+            }
+            
+            if(thinking) {
+                ProgressView().onAppear() {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                        thinking = false
+                        bubbles.append("I don't know, Marc hasn't implemented this part yet.")
+                    }
+                }
+            }
+            Spacer()
+            Text("How can I help you?")
+            TextField(
+                    "Hello, ...",
+                    text: $question
+            ).padding()
+                .border(.gray)
+                .padding()
+            Button(action: submit) {
+                Text("Send")
+            }
         }
         .padding()
+    }
+    
+    func submit() {
+        bubbles.append(question)
+        thinking = true
+        question = ""
     }
 }
 

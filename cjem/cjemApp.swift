32812ee886d9d842226d17fdc0ffe10b20375674
Marc G Gauthier <marcg.gauthier@gmail.com>
2023-11-01 13:08:28 +0100

    Initial Commit

---
+++ cjem/cjemApp.swift
@@ -0,0 +1,17 @@
+//
+//  cjemApp.swift
+//  cjem
+//
+//  Created by Marc G Gauthier on 01/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct cjemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
